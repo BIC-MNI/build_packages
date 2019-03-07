308aefbcf722f8e9595b1479efef2deea15b186c
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-03-07 13:39:44 -0500

    Generalize prepare docker build images

--- prepare_docker_build_images.sh
+++ prepare_docker_build_images.sh
@@ -2,14 +2,10 @@
 
 
 # prepare all docker images
-for m in build_centos_7.3_x64 \
-            build_debian_8_x64 \
-            build_fedora_25_x64 \
-            build_ubuntu_14.04_x32 \
-            build_ubuntu_14.04_x64 \
-            build_ubuntu_16.04_x32 \
-            build_ubuntu_16.04_x64 \
-            build_ubuntu_18.04_x64 
+for m in build_centos_* \
+            build_debian_* \
+            build_fedora_* \
+            build_ubuntu_* \
 do
 docker build -t minc-${m} $m
 done
