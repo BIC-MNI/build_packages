29e2bac303adf718f31e128dbe070fdc58bb8834
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-03-08 08:48:51 -0500

    Fix trailing slash

--- prepare_docker_build_images.sh
+++ prepare_docker_build_images.sh
@@ -5,7 +5,7 @@
 for m in build_centos_* \
             build_debian_* \
             build_fedora_* \
-            build_ubuntu_* \
+            build_ubuntu_*
 do
 docker build -t minc-${m} $m
 done
