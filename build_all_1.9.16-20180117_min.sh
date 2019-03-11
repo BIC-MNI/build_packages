e2a6fdd65061ec0a1d5deb65efaf495711ad4c9a
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-03-11 14:20:42 -0400

    Cleanup bash formatting, add logging

--- build_all_1.9.16-20180117_min.sh
+++ build_all_1.9.16-20180117_min.sh
@@ -7,11 +7,10 @@ v=v2
 
 # build RPM-based packages
 for m in centos_7.3_x64 ;do
-./build_${v}_min.sh minc-build_$m RPM
+  ./build_${v}_min.sh minc-build_$m RPM
 done
 
 # build DEB-based packages ubuntu_14.04_x64 ubuntu_16.04_x64
 for m in ubuntu_16.04_x64 ;do
-./build_${v}_min.sh minc-build_$m DEB
+  ./build_${v}_min.sh minc-build_$m DEB
 done
-
